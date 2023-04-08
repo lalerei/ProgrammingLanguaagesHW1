ec8393d3fb281f3dba0fd79216b19982f8ea5cbc
lalerei <lale.ley@gmail.com>
2023-04-08 23:39:34 +0300

    Task 6 completed. Задача 6 решена.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа 
 // и выдаёт, какое число большее
 // e.g. a = 2 b = 10 -> max = 10
-
+/*
 Console.WriteLine("Input the first number : ");
 int num1 = Convert.ToInt32 (Console.ReadLine());
 
@@ -16,6 +16,7 @@ else
 {
   Console.WriteLine("The largest number is : " + num2);
 }
+*/
 
 
 // Задача 4: Напишите программу, которая принимает на вход три числа 
@@ -53,6 +54,17 @@ else
 // является ли число чётным (делится ли оно на два без остатка).
 // e.g. 4 -> да        7 -> нет
 
+Console.WriteLine("Input the number you want to check: ");
+int numberA = Convert.ToInt32 (Console.ReadLine());
+
+if (numberA%2 == 0)
+{
+  Console.WriteLine("YES, the number is even");
+}
+else
+{
+  Console.WriteLine("NO, the number is not even, it is odd");
+}
 
 
 // Задача 8: Напишите программу, которая на вход принимает число (N), 
